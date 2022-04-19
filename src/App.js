6590a98389f9bebb4b49ab9122b984d600ecea32
Yulchiko <Yulchik.82@i.ua>
2022-04-19 22:26:26 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = (state, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case 'addCat':
+            return {...state, cats: [...state.cats, {name: payload, id: Date.now()}]}
+        case 'delCat':
+            return {...state, cats: state.cats.filter(cat => cat.id !== payload)}
+        case 'addDog':
+            return {...state, dogs: [...state.dogs, {name: payload, id: Date.now()}]}
+        case 'delDog':
+            return {...state, dogs: state.dogs.filter(dog => dog.id !== payload)}
+
+        default:
+            throw new Error('Error')
+    }
 }
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, {cats: [], dogs: []});
+    const [catValue, setCatValue] = useState('')
+    const [dogValue, setDogValue] = useState('')
+
+    const clickCat = () => {
+        dispatch({type: 'addCat', payload: catValue})
+        setCatValue('')
+    }
+
+    const clickDog = () => {
+        dispatch({type: 'addDog', payload: dogValue})
+        setCatValue('')
+    }
+    return (
+        <div >
+            <div >
+                <label>Cat name: <input type="text" onChange={({target}) => setCatValue(target.value)}
+                                        value={catValue}/></label>
+                <button onClick={clickCat}>Save</button>
+                <label>Dog name: <input type="text" onChange={({target}) => setDogValue(target.value)}
+                                        value={dogValue}/></label>
+                <button onClick={clickDog}>Save</button>
+            </div>
+            <hr/>
+            <div style={{  marginLeft:'20px'}}>
+                <div style={{display:'flex'}}>
+                    {state.cats.map(cat=>(
+                        <div key={cat.id}>
+                            {cat.name}
+                            <button onClick={() => dispatch({type: 'delCat', payload: cat.id})}>Delete</button>
+                        </div>
+                    ))}
+                    {state.dogs.map(dog=>(
+                        <div key={dog.id}>
+                            {dog.name}
+                            <button onClick={() => dispatch({type: 'delDog', payload: dog.id})}>Delete</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
